7bd5416856574bb75ec33d976329710ec0e37c36
Mohamed Omar <mohamed.omar.freelancer@gmail.com>
2022-07-06 15:10:41 +0200

    init: Vue-i18n

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ $t("content") }}</p>
+</template>
+
+<script>
+
+export default ({
+  name: "HelloI18n",
+});
+</script>
+
+<i18n>
+{
+  "en": {
+    "content": "English Content."
+  },
+  "fr": {
+    "content": "Contenu en français."
+  },
+  "ar": {
+    "content": "المحتوى باللغة العربية."
+  }
+}
+</i18n>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>{{ $t('message') }}</h1>
+    <div>
+      <select v-model="$i18n.locale" @change="detectLanguage($event)">
+        <option
+          v-for="langnguage in languages"
+          :key="langnguage"
+          :value="langnguage">
+          {{ langnguage }}
+        </option>
+      </select>
+    </div>
+    <HelloI18n />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloI18n from "@/components/HelloI18n.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloI18n,
+  },
+  data(){
+    return {
+      languages: ['en', 'fr', 'ar']
+    }
+  },
+  methods: {
+    detectLanguage(event){
+      event.target.value === 'ar' ? document.body.setAttribute('dir', 'rtl') : document.body.setAttribute('dir', 'ltr');
+    }
+  }
+};
+</script>
